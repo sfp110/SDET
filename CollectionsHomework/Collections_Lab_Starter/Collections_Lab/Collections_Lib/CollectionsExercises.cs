79338eba05e2d65d92df5cf30a840d76426bce9c
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-08-09 14:25:11 +0100

    Add both the Collections Homework as well as Kata Exercises onto the SDET file

---
+++ CollectionsHomework/Collections_Lab_Starter/Collections_Lab/Collections_Lib/CollectionsExercises.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections_Lib
+{
+    public class CollectionsExercises
+    {
+
+        /* removes and returns the next num entries in the queue, as a comma separated string */
+        public static string NextInQueue(int num, Queue<string> queue)
+        {
+            string result = string.Empty;
+
+            if (num == 0 || num < 0)
+            {
+                return string.Empty;
+            }
+
+            while (num-- > 0 && queue.Count > 0)
+            {
+                result += queue.Dequeue() + ", ";
+            }
+
+            //I had to remove 2 because I was adding the comma and the space afterwards!
+            return result.Remove(result.Length-2, 2);
+        }
+
+        /* uses a Stack to create and return array of ints in reverse order to the one supplied */
+        public static int[] Reverse(int[] original)
+        {
+            int[] reversed = new int[original.Length];
+
+            var stack = new Stack<int>();
+
+            foreach(var item in original)
+            {
+                //Push the ints to the stack
+                stack.Push(item);
+            }
+
+            for(int i = 0; i < original.Length; i++)
+            {
+                reversed[i] = stack.Pop();
+            }
+            
+            return reversed;
+
+        }
+        // using a Dictionary, counts and returns (as a string) the occurence of the digits 0-9 in the given string
+        public static string CountDigits(string input)
+        {
+            string result = "";
+
+            string newNumbers = string.Empty;
+
+            var dictionary = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                    newNumbers += input[i];
+            }
+
+            foreach (var x in newNumbers)
+            {
+                if (dictionary.ContainsKey(x))
+                {
+                    dictionary[x]++;
+                }
+                else
+                {
+                    dictionary.Add(x, 1);
+                }
+            }
+
+            
+            foreach (var entry in dictionary)
+            {
+                result += entry + "";
+            }
+            return result; //might need to concatonate the result
+        }
+    }
+}
---
+++ kataExercises/kataExercises/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace kataExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(CurrencySplitter(29.37));
+        }
+
+
+        public static string CurrencySplitter(double totalAmout) 
+        {
+
+            double remaining = totalAmout;
+
+            int numberOfFifties = (int)(totalAmout / 50);
+
+            remaining -= numberOfFifties * 50;
+
+            int numberOfTwenties = (int)(remaining / 20);
+
+            remaining -= numberOfTwenties * 20;
+
+            int numberOfTens = (int)(remaining / 10);
+
+            remaining -= numberOfTens * 10;
+
+            int numberOfFivers = (int)(remaining / 5);
+
+            remaining -= numberOfFivers * 5;
+
+            int numberOf2PoundCoins = (int)(remaining / 2);
+
+            remaining -= numberOf2PoundCoins * 2;
+
+            int numberOf1PoundCoins = (int)(remaining / 1);
+
+            remaining -= numberOf1PoundCoins * 1;
+
+            int numberOf50PiCoins = (int)(remaining / 0.5);
+
+            remaining -= numberOf50PiCoins * 0.5;
+
+            int numberOf20PiCoins = (int)(remaining / 0.2);
+
+            remaining -= numberOf20PiCoins * 0.2;
+
+            int numberOf10PiCoins = (int)(remaining / 0.1);
+
+            remaining -= numberOf10PiCoins * 0.1;
+
+            int numberOf05PiCoins = (int)(remaining / 0.05);
+
+            remaining -= numberOf05PiCoins * 0.05;
+
+            int numberOf02PiCoins = (int)(remaining / 0.02);
+
+            remaining -= numberOf10PiCoins * 0.02;
+
+            int numberOf01PiCoins = (int)(remaining / 0.01);
+
+            remaining -= numberOf01PiCoins * 0.01;
+
+            int a = 15;
+            int b = -5;
+
+            Console.WriteLine(a += b); // a is now 10
+            Console.WriteLine(a = +b); // a is now -5
+
+            Console.WriteLine("The total amount is : " + "£" + totalAmout);
+
+            return $"£50: {numberOfFifties}\n£20: {numberOfTwenties}\n£10: {numberOfTens}\n £5: {numberOfFivers}\n £2: {numberOf2PoundCoins}\n £1: {numberOf1PoundCoins}\n50p: {numberOf50PiCoins}\n20p: {numberOf20PiCoins}\n10p: {numberOf10PiCoins}\n 5p: {numberOf05PiCoins}\n 2p: {numberOf02PiCoins}\n 1p: {numberOf01PiCoins}";
+        }
+    }
+}
